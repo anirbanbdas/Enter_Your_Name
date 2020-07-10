3bce4d1eefb4560a2c2c9b43eba8d675b87790fe
Anirban Das <47988137+anirbanbdas@users.noreply.github.com>
2020-07-10 16:11:52 +0530

    removed some functions and organized it

--- enter-your-name.c
+++ enter-your-name.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+//Calculate the summation
+//Check for prime
+//Number Of Characters
+//Number of Possible Permutations
+//Permutation of name
+//Morse code of name
+//Braille code of name
+//Compare name with other name
 void main()
 {
   char* p;
@@ -13,80 +21,6 @@ void main()
   prime_check(sum);
 
 
+return 0;
+}
 
-}
-int dictionary_number(char* p)
-{   int s=0;
-    int i;
-    while(*p!='\0')
-    {       
-      for(i=65;i<=90;i++)
-      {
-          if(*p=='i' || *p=='i+32')
-          {
-            s=s+(i-64); 
-          }
-      }
-      p++;
-    }
-    return s;
-} 
-void prime_check(int a)
-{  int i;
-   if(a==1)
-   {
-       printf("You neither Prime nor Non-Prime");
-   }
-   else
-   {
-       for(i=1;i<=a;i++)
-       {
-           if(a%i==0)
-           {
-               printf("You are not Prime");
-           }
-           else
-           {
-               printf("You are Prime");
-           }
-       }
-   }
-}
-void lexci_permut(char* p,int n)
-{   
-    int i=n-1;
-    while(i>0 && strcmp(s[i-1],s[i])>=0)
-    {
-        i--;
-    }
-    int j=n-1;
-    while(strcmp(s[j-1],s[j])>=0)
-    {
-        j--;
-    }
-    swap(&s[i-1],&s[j]);
-    reverse();
-
-    
-
-    
-}
-void swap(char* a,char* b)
-{
-    char tmp=*a;
-    *a=*b;
-    *b=tmp;
-}
-void reverse(char* a)
-{
-  int i,j;
-  while(*a!='\0')
-  {
-    
-  }
-  
-}
-void alpha_order(Char* a)
-{
-   
-}
