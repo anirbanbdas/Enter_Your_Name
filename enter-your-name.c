8469edf85015a436dca20326e11d681e305fe25a
Anirban Das <47988137+anirbanbdas@users.noreply.github.com>
2020-07-26 23:39:55 +0530

    fixed the summation problem

--- enter-your-name.c
+++ enter-your-name.c
@@ -10,42 +10,40 @@
 //Braille code of name
 
 void prime_check(int);
-void main()
+int main()
 { printf("Enter Your Name:");
   char* name=(char*)malloc(sizeof(char)*1024);
   scanf("%s",name);
   printf("The number of letters in your name is:%d\n",strlen(name));
-  int i=0;
-  int sum=0;
-  while(name[i]=!'\0')
-  {
-      for(int j=65;j<=90;j++)
-      {
-          if(name[i]==j)
-          {
-             sum=sum+(j-64);
-          }
-      }
-      i++;
-  }
-  printf("The summation is:%d\n",sum);
-  prime_check(sum);
+    int i=0;
+    int sum=0;
+    while(name[i]!='\0')
+    {
+        for(int j=97;j<=122;j++)
+           {
+             if(name[i]==j || name[i]==(j-32))
+                  sum=sum+(j-96);
+           }
+        i++;
+    }
 
+   printf("The summation is %d\n",sum);
+   prime_check(sum);
 
-
+   return 0;
 }
 void prime_check(int sum)
 {
     int flag=0;
-    if(sum==1)
-        printf("You are neither Prime nor Non Prime\n");
-    else
-    {
-        for(int i=2;i<=sum;i++)
+    for(int i=2;i<=sum;i++)
         {
             if(sum%i==0)
                 flag++;
         }
+    if(sum==1)
+        printf("You are neither Prime nor Non Prime\n");
+    else
+    {
         if(flag==0)
             printf("You are Prime\n");
         else
