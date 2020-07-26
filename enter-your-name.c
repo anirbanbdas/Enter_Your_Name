8b71be69ecfb4b54dd57f04ce82622ff07b5cc10
Anirban Das <47988137+anirbanbdas@users.noreply.github.com>
2020-07-26 22:15:41 +0530

    add function prime and some features

--- enter-your-name.c
+++ enter-your-name.c
@@ -1,26 +1,55 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+//Number Of Characters
 //Calculate the summation
 //Check for prime
-//Number Of Characters
 //Number of Possible Permutations
 //Permutation of name
 //Morse code of name
 //Braille code of name
-//Compare name with other name
+
+void prime_check(int);
 void main()
-{
-  char* p;
-  int sum;
-  p=(char*)malloc(sizeof(char)*1024);
-  printf("Enter Your Name:");
-  scanf("%s",p);
-  sum=dictionary_number(p);
-  printf("\nYour letter summation is %d\n",sum);
+{ printf("Enter Your Name:");
+  char* name=(char*)malloc(sizeof(char)*1024);
+  scanf("%s",name);
+  printf("The number of letters in your name is:%d\n",strlen(name));
+  int i=0;
+  int sum=0;
+  while(name[i]=!'\0')
+  {
+      for(int j=65;j<=90;j++)
+      {
+          if(name[i]==j)
+          {
+             sum=sum+(j-64);
+          }
+      }
+      i++;
+  }
+  printf("The summation is:%d\n",sum);
   prime_check(sum);
 
 
-return 0;
+
+}
+void prime_check(int sum)
+{
+    int flag=0;
+    if(sum==1)
+        printf("You are neither Prime nor Non Prime\n");
+    else
+    {
+        for(int i=2;i<=sum;i++)
+        {
+            if(sum%i==0)
+                flag++;
+        }
+        if(flag==0)
+            printf("You are Prime\n");
+        else
+            printf("You are Non Prime\n");
+    }
 }
 
