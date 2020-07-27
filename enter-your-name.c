2cfb845c273a87b22f1d42abac0cc2d7512a84ec
Anirban Das <47988137+anirbanbdas@users.noreply.github.com>
2020-07-27 23:12:27 +0530

    added permutation to main

--- enter-your-name.c
+++ enter-your-name.c
@@ -1,15 +1,41 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include "permutation.c"
+//Basic
+
 //Number Of Characters
 //Calculate the summation
 //Check for prime
+
+//Will be asked by the user
+
 //Number of Possible Permutations
 //Permutation of name
 //Morse code of name
 //Braille code of name
 
-void prime_check(int sum);
+
+void prime_check(int sum)
+{
+    int flag=0;
+    for(int i=2;i<sum;i++)
+        {
+            if(sum%i==0)
+                {flag++;
+                }
+        }
+    if(sum==1)
+        printf("You are neither Prime nor Non Prime\n");
+    else
+    {
+        if(flag==0)
+            printf("You are Prime\n");
+        else
+            printf("You are Non Prime\n");
+    }
+}
+
 int main()
 { printf("Enter Your Name:");
   char* name=(char*)malloc(sizeof(char)*1024);
@@ -29,26 +55,20 @@ int main()
 
    printf("The summation is %d\n",sum);
    prime_check(sum);
+   printf("Do you want to know the possible permutation of your name(Y/N):");
+   char que_one;
+   scanf("%c",&que_one);
+   if (que_one=='Y' || que_one=='y')
+    {  int n=strlen(name);
+        char* name_cpy=(char*)malloc(1024*sizeof(char));
+        strcpy(name_cpy,name);
+        strlwr(name_cpy);
+       do
+       {
+         printf("%s\n",name_cpy);
+       }while(next_permutation(n,name_cpy));
+    }
 
    return 0;
-}
-void prime_check(int sum)
-{
-    int flag=0;
-    for(int i=2;i<sum;i++)
-        {
-            if(sum%i==0)
-                {flag++;
-                }
-        }
-    if(sum==1)
-        printf("You are neither Prime nor Non Prime\n");
-    else
-    {
-        if(flag==0)
-            printf("You are Prime\n");
-        else
-            printf("You are Non Prime\n");
-    }
 }
 
